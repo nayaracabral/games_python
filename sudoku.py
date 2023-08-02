813e9550565493539d6a2ac9f6f0bc29f4753e38
Nayara D <130619243+nayaracabral@users.noreply.github.com>
2023-08-02 01:07:49 -0300

    Create sudoku.py
    O Sudoku é um jogo de lógica em que o objetivo é preencher uma grade 9x9 com números de 1 a 9, de modo que cada linha, coluna e região 3x3 contenha apenas um número de cada valor possível. 
    
    #Para vencer o jogo Sudoku usando o código anterior, siga os seguintes passos:
    
    1. Observe cuidadosamente o tabuleiro inicial, identificando quais números já estão preenchidos e onde estão localizados.
    
    2. Analise cada região 3x3 do tabuleiro para identificar quais números estão faltando e onde eles podem ser colocados.
    
    3. Considere cada linha e coluna individualmente e determine quais números estão faltando. Use a lógica de que cada número só pode aparecer uma vez em cada linha ou coluna.
    
    4. Use a função `solve` do código para preencher as células vazias do tabuleiro com números de 1 a 9. A solução será encontrada automaticamente pelo programa.
    
    5. Verifique a solução encontrada pela função `solve` usando a função `print_board`, e confira se todas as linhas, colunas e regiões 3x3 contêm cada número de 1 a 9 exatamente uma vez.
    
    
    Se alguma região, linha ou coluna não contiver cada número de 1 a 9 exatamente uma vez, ou se houver dois ou mais números iguais na mesma linha, coluna ou região, isso significa que a solução está incorreta e você deve rever os passos acima. Se tudo estiver certo, parabéns, você venceu o jogo!
    
    # Rodar code
    
    
    Para executar o código, basta copiar e colar o código em um arquivo chamado `sudoku.py`. Em seguida, abra um terminal na mesma pasta do arquivo e digite `python sudoku.py`. O programa deve então ser executado.

---
+++ sudoku.py
@@ -0,0 +1,79 @@
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+
+    return None
+
+def valid(board, num, pos):
+    # Verifica a linha
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Verifica a coluna
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Verifica o quadrante 3x3
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+]
+
+print("Sudoku inicial:")
+print_board(board)
+solve(board)
+print("\nSudoku resolvido:")
+print_board(board)
